f4db05c798df8bc8e66bec975b5e262da1e609a8
york <2762348652@qq.com>
2023-11-17 17:55:17 +0800

    2023.11.17 更改脚本，一次跑两个数据集

--- Sketch-topk/Sketch-topk/run.sh
+++ Sketch-topk/Sketch-topk/run.sh
@@ -1,22 +1,45 @@
 rm result.csv
 make
-echo "MEM,func,k,AAE,ARE,_sum,throughput" >> result.csv
-ruleDir='/home/york/dataset/data/1.dat'
+echo "MEM,func,k,AAE,ARE,_sum,throughput(insert),throughput(query)" >> result.csv
 
+ruleDir1='/home/york/dataset/data'
 for MEM in $(seq 100 100 1000)
 do
     for K in 10 50 $(seq 100 100 1000)
     do
-        cmd="./cuckoo -d $ruleDir -m $MEM -k $K"
-        echo $cmd
-        eval $cmd
-        pid=$!
-        wait $pid
-        if ps -p $pid > /dev/null; then
-            kill -9 $pid
-        fi
-        echo "done"
-        sleep 1
+        for file in $ruleDir1/*.dat
+        do
+            cmd="./cuckoo -d $file -m $MEM -k $K"
+            echo $cmd
+            eval $cmd
+            pid=$!
+            wait $pid
+            if ps -p $pid > /dev/null; then
+                kill -9 $pid
+            fi
+            echo "done"
+            sleep 1
+        done
     done
 done
 
+ruleDir2='/home/york/dataset/zipf'
+for MEM in $(seq 100 100 1000)
+do
+    for K in 10 50 $(seq 100 100 1000)
+    do
+        for file in $ruleDir2/zipf_*.dat
+        do
+            cmd="./cuckoo -d $file -m $MEM -k $K"
+            echo $cmd
+            eval $cmd
+            pid=$!
+            wait $pid
+            if ps -p $pid > /dev/null; then
+                kill -9 $pid
+            fi
+            echo "done"
+            sleep 1
+        done
+    done
+done
